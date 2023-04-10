397c715c3ac3bade4d455b4a771680c0b862f224
Felix Belthoise <fbelthoi@z3r7p5.42lyon.fr>
2023-04-10 14:04:19 +0200

    cleaned repo

--- set_mac.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/${LMLX_LINUX}/${LMLX_MACOS}/g' Makefile
-sed -i 's/${IMLX_LINUX}/${IMLX_MACOS}/g' Makefile
-sed -i 's/_KEY_LIN/_KEY_MAC/g' srcs/*.c srcs/*/*.c
-sed -i 's+mlx_linux/+mlx/+g' Makefile
